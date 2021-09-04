061cfd85cf8f95a1d7b16c03362114511729fae0
Joel Thompson <joel.thompson@circleci.com>
2021-09-04 11:04:49 -0700

    adding some more style

--- src/components/Bikes.tsx
+++ src/components/Bikes.tsx
@@ -1,9 +1,14 @@
 import { Box, Button } from '@chakra-ui/react';
 import { GiAmericanShield } from 'react-icons/gi';
 
-function showMessage() {
-  alert('go ride you fool');
-}
+// function showMessage(message = 'go ride you fool') {
+//   alert(message);
+//   // return undefined;
+// }
+
+const showMessage = (message = 'go ride you fool') => {
+  alert(message);
+};
 
 export default function Bikes() {
   return (
@@ -14,12 +19,16 @@ export default function Bikes() {
         bg={'blue.400'}
         w={300}
         h={400}
+        mb={20}
+        mt={4}
       >
         Hello
       </Box>
       <Button
         leftIcon={<GiAmericanShield />}
-        onClick={showMessage}
+        onClick={() => {
+          showMessage('foo');
+        }}
         size="lg"
         colorScheme="blue"
       >
