743f6f882e3934736a429dc9a8f1560ceb499334
Joel Thompson <joel.thompson@circleci.com>
2021-09-04 08:01:25 -0700

    just putting a bunch of stuff in there

--- src/components/Bikes.tsx
+++ src/components/Bikes.tsx
@@ -1,7 +1,30 @@
+import { Box, Button } from '@chakra-ui/react';
+import { GiAmericanShield } from 'react-icons/gi';
+
+function showMessage() {
+  alert('go ride you fool');
+}
+
 export default function Bikes() {
   return (
-    <div>
-      <h2>Bikes are fun</h2>
-    </div>
+    <>
+      <h2>Bikes are funs</h2>
+      <Box
+        _hover={{ backgroundColor: 'green.400' }}
+        bg={'blue.400'}
+        w={300}
+        h={400}
+      >
+        Hello
+      </Box>
+      <Button
+        leftIcon={<GiAmericanShield />}
+        onClick={showMessage}
+        size="lg"
+        colorScheme="blue"
+      >
+        I can do this all day
+      </Button>
+    </>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,10 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 import { ChakraProvider } from '@chakra-ui/react';
-import { extendTheme } from '@chakra-ui/react';
-import theme from './theme';
-
-const extendedTheme = extendTheme({ theme });
 
 ReactDOM.render(
   <React.StrictMode>
-    <ChakraProvider theme={extendedTheme}>
+    <ChakraProvider>
       <App />
     </ChakraProvider>
   </React.StrictMode>,
