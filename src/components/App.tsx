3c4e5f9cb41328a6114f6f62b33d4b814836d75d
Joel Thompson <joel.thompson@circleci.com>
2021-09-04 11:05:41 -0700

    remove comment

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,15 +4,6 @@ import TopNav from './TopNav';
 import Bikes from './Bikes';
 import Home from './Home';
 
-// This site has 3 pages, all of which are rendered
-// dynamically in the browser (not server rendered).
-//
-// Although the page does not ever refresh, notice how
-// React Router keeps the URL up to date as you navigate
-// through the site. This preserves the browser history,
-// making sure things like the back button and bookmarks
-// work properly.
-
 export default function App() {
   return (
     <Router>
