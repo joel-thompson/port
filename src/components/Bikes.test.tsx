8171e15068994b6e565b023cb53108f65ffd5194
Joel Thompson <joel.thompson@circleci.com>
2021-10-08 14:03:08 -0700

    fix tests

--- src/components/Bikes.test.tsx
+++ src/components/Bikes.test.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import Bikes from './Bikes';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 test('renders bikes page', () => {
-  render(<Bikes />);
+  render(
+    <Router>
+      <Bikes />
+    </Router>,
+  );
   const pElement = screen.getByText('Bikes are fun');
   expect(pElement).toBeInTheDocument();
 });
