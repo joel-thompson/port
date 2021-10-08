59c2a8df406a329ddf75899883fb214418183b8f
Joel Thompson <joel.thompson@circleci.com>
2021-10-08 07:25:57 -0700

    front cente rear center calc

--- src/components/Bikes.tsx
+++ src/components/Bikes.tsx
@@ -1,41 +1,75 @@
-import { Box, Button } from '@chakra-ui/react';
+import {
+  Box,
+  Button,
+  NumberInput,
+  NumberInputField,
+  NumberInputStepper,
+  NumberIncrementStepper,
+  NumberDecrementStepper,
+  Heading,
+} from '@chakra-ui/react';
+import React from 'react';
+
 import { GiAmericanShield } from 'react-icons/gi';
 
-const showMessage = (message = 'go ride you fool') => {
-  alert(message);
-};
-
-const BikeBox = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <Box
-      _hover={{ backgroundColor: 'green.400' }}
-      bg={'blue.400'}
-      w={300}
-      h={400}
-      mb={20}
-      mt={4}
-    >
-      {children}
-    </Box>
-  );
-};
+// const BikeBox = ({ children }: { children: React.ReactNode }) => {
+//   return (
+//     <Box
+//       _hover={{ backgroundColor: 'green.400' }}
+//       bg={'blue.400'}
+//       w={300}
+//       h={400}
+//       mb={20}
+//       mt={4}
+//     >
+//       {children}
+//     </Box>
+//   );
+// };
 
 export default function Bikes() {
+  const [wheelbase, setWheelbase] = React.useState(0);
+  const [chainstay, setChainstay] = React.useState(0);
   return (
     <>
-      <h2>Bikes are fun</h2>
-      <BikeBox>Hello</BikeBox>
-      <Button
-        ml={2}
-        leftIcon={<GiAmericanShield />}
-        onClick={() => {
-          showMessage("No, I don't think I will");
+      <Heading>Front center / rear center ratio calculator</Heading>
+      <p>Wheelbase</p>
+      <NumberInput
+        size="sm"
+        min={0}
+        defaultValue={0}
+        onChange={(value) => {
+          setWheelbase(parseInt(value));
         }}
-        size="lg"
-        colorScheme="blue"
       >
-        I can do this all day
-      </Button>
+        <NumberInputField />
+        <NumberInputStepper>
+          <NumberIncrementStepper />
+          <NumberDecrementStepper />
+        </NumberInputStepper>
+      </NumberInput>
+
+      <p>Rear center / chainstay</p>
+      <NumberInput
+        size="sm"
+        min={0}
+        defaultValue={0}
+        onChange={(value) => {
+          setChainstay(parseInt(value));
+        }}
+      >
+        <NumberInputField />
+        <NumberInputStepper>
+          <NumberIncrementStepper />
+          <NumberDecrementStepper />
+        </NumberInputStepper>
+      </NumberInput>
+
+      <br />
+      <p>front center: {wheelbase - chainstay}</p>
+      <p>
+        front center / rear center ratio: {(wheelbase - chainstay) / chainstay}
+      </p>
     </>
   );
 }
