aeaedbbb04a7596ebb5a1fa7c981bfc19ab1653c
Joel Thompson <joel.thompson@circleci.com>
2022-01-26 17:34:18 -0800

    add a fun grid

--- src/components/Bikes.tsx
+++ src/components/Bikes.tsx
@@ -2,21 +2,6 @@ import { Heading, Divider, Text } from '@chakra-ui/react';
 import { Link, Route, Switch } from 'react-router-dom';
 import MultiFrontRearCenterCalculator from './bikes/MultiFrontRearCenterCalculator';
 
-// const BikeBox = ({ children }: { children: React.ReactNode }) => {
-//   return (
-//     <Box
-//       _hover={{ backgroundColor: 'green.400' }}
-//       bg={'blue.400'}
-//       w={300}
-//       h={400}
-//       mb={20}
-//       mt={4}
-//     >
-//       {children}
-//     </Box>
-//   );
-// };
-
 export default function Bikes() {
   return (
     <>
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,18 +1,47 @@
-import { Box } from '@chakra-ui/react';
+import { Grid, GridItem } from '@chakra-ui/react';
 import React from 'react';
 
-const SomeComp = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <Box _hover={{ backgroundColor: 'blue.200' }} bg="pink">
-      {children}
-    </Box>
-  );
-};
+// const SomeComp = ({ children }: { children: React.ReactNode }) => {
+//   return (
+//     <Box _hover={{ backgroundColor: 'blue.200' }} bg="pink">
+//       {children}
+//     </Box>
+//   );
+// };
 
 export default function Home() {
   return (
     <>
-      <SomeComp>Hello</SomeComp>
+      <Grid
+        w="60"
+        templateColumns="repeat(2, 1fr)"
+        templateRows="repeat(2, 1fr)"
+      >
+        <GridItem
+          w="100%"
+          h="10"
+          bg="blue.500"
+          _hover={{ backgroundColor: 'blue.200' }}
+        />
+        <GridItem
+          w="100%"
+          h="10"
+          bg="red.500"
+          _hover={{ backgroundColor: 'red.200' }}
+        />
+        <GridItem
+          w="100%"
+          h="10"
+          bg="green.500"
+          _hover={{ backgroundColor: 'green.200' }}
+        />
+        <GridItem
+          w="100%"
+          h="10"
+          bg="purple.500"
+          _hover={{ backgroundColor: 'purple.200' }}
+        />
+      </Grid>
       <br />
       <p>My Name is Joel and this is my website</p>
     </>
