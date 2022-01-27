e048b2f8c6e682dccda1fbb06fa5561e77ae423a
Joel Thompson <joel.thompson@circleci.com>
2022-01-26 17:53:52 -0800

    some more noodling

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -14,33 +14,16 @@ export default function Home() {
     <>
       <Grid
         w="60"
-        templateColumns="repeat(2, 1fr)"
-        templateRows="repeat(2, 1fr)"
+        templateColumns="repeat(2, 50px)"
+        templateRows="repeat(2, 50px) 100px"
+        gap="1"
       >
-        <GridItem
-          w="100%"
-          h="10"
-          bg="blue.500"
-          _hover={{ backgroundColor: 'blue.200' }}
-        />
-        <GridItem
-          w="100%"
-          h="10"
-          bg="red.500"
-          _hover={{ backgroundColor: 'red.200' }}
-        />
-        <GridItem
-          w="100%"
-          h="10"
-          bg="green.500"
-          _hover={{ backgroundColor: 'green.200' }}
-        />
-        <GridItem
-          w="100%"
-          h="10"
-          bg="purple.500"
-          _hover={{ backgroundColor: 'purple.200' }}
-        />
+        <GridItem bg="blue.500" _hover={{ backgroundColor: 'blue.200' }} />
+        <GridItem bg="red.500" _hover={{ backgroundColor: 'red.200' }} />
+        <GridItem bg="green.500" _hover={{ backgroundColor: 'green.200' }} />
+        <GridItem bg="purple.500" _hover={{ backgroundColor: 'purple.200' }} />
+        <GridItem bg="blue.500" _hover={{ backgroundColor: 'blue.200' }} />
+        <GridItem bg="blue.500" _hover={{ backgroundColor: 'blue.200' }} />
       </Grid>
       <br />
       <p>My Name is Joel and this is my website</p>
