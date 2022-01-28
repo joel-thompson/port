227e6f0ab843121edfb0865d55be16264a8f2984
Joel Thompson <joel.thompson@circleci.com>
2022-01-28 15:09:03 -0800

    arrange stuff

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,4 +1,4 @@
-import { Grid, GridItem } from '@chakra-ui/react';
+import { Grid, GridItem, Text } from '@chakra-ui/react';
 import React from 'react';
 
 // const SomeComp = ({ children }: { children: React.ReactNode }) => {
@@ -14,18 +14,50 @@ export default function Home() {
     <>
       <Grid
         w="auto"
-        h="72"
-        templateColumns="repeat(2, 1fr)"
-        templateRows="repeat(2, 1fr) 2fr"
+        h="96"
+        templateColumns="[start] repeat(2, 1fr) [mid] repeat(2, 1fr) [end]"
+        templateRows="[start] repeat(2, 1fr) 2fr 2fr [last]"
         gridColumnGap="30px"
         gridRowGap="20px"
       >
-        <GridItem bg="blue.500" _hover={{ backgroundColor: 'blue.200' }} />
-        <GridItem bg="red.500" _hover={{ backgroundColor: 'red.200' }} />
-        <GridItem bg="green.500" _hover={{ backgroundColor: 'green.200' }} />
-        <GridItem bg="purple.500" _hover={{ backgroundColor: 'purple.200' }} />
-        <GridItem bg="blue.500" _hover={{ backgroundColor: 'blue.200' }} />
-        <GridItem bg="blue.500" _hover={{ backgroundColor: 'blue.200' }} />
+        <GridItem
+          gridColumn="start / mid"
+          bg="blue.500"
+          _hover={{ backgroundColor: 'blue.200' }}
+        />
+        <GridItem
+          gridColumn="mid / end"
+          bg="red.500"
+          _hover={{ backgroundColor: 'red.200' }}
+        />
+        <GridItem
+          gridColumn="start / mid"
+          bg="green.500"
+          _hover={{ backgroundColor: 'green.200' }}
+        />
+        <GridItem
+          gridColumn="mid / end"
+          bg="purple.500"
+          _hover={{ backgroundColor: 'purple.200' }}
+        />
+        <GridItem
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          gridColumn="start / end"
+          bg="yellow.500"
+          _hover={{ outline: 'solid', outlineColor: 'purple.200' }}
+        >
+          <Text fontSize="lg" fontWeight="bold">
+            Hi
+          </Text>
+        </GridItem>
+        <GridItem
+          gridRowEnd="last"
+          gridColumn="2 / span 2"
+          bg="yellowgreen"
+          _hover={{ outline: 'dashed', outlineColor: 'purple.200' }}
+        />
       </Grid>
       <br />
       <p>My Name is Joel and this is my website</p>
