935fddf15d4ea5cb11cb211e39b955b6efeefec4
Joel Thompson <joel.thompson@circleci.com>
2022-12-30 14:33:51 -0800

    more resume updates

--- src/components/resume/Resume.tsx
+++ src/components/resume/Resume.tsx
@@ -1,16 +1,63 @@
-import { resumeData } from './resumeData';
+import {
+  Box,
+  Card,
+  Divider,
+  Heading,
+  HStack,
+  ListItem,
+  StackDivider,
+  UnorderedList,
+  VStack,
+  Text,
+  Stack,
+} from '@chakra-ui/react';
+import { resumeData, Job, Strength } from './resumeData';
 
 export default function Home() {
   return (
-    <>
-      <p>{resumeData.name}</p>
+    <VStack spacing="2" align="flex-start">
+      <Heading>{resumeData.name}</Heading>
+
+      <Divider />
+      <Text>Experience</Text>
       {resumeData.jobHistory.map((job) => (
-        <>
-          <p>{job.company}</p>
-          <p>{job.title}</p>
-          <p>{job.time}</p>
-        </>
+        <ShowJob job={job} />
       ))}
-    </>
+
+      <Divider />
+      <Text>Strengths</Text>
+      {resumeData.strengths.map((strength) => (
+        <ShowStrength strength={strength} />
+      ))}
+    </VStack>
   );
 }
+
+const ShowJob = ({ job }: { job: Job }) => {
+  return (
+    <Stack direction="row" borderWidth="1px" shadow="sm">
+      <Box p={5} w="60">
+        <Heading fontSize="xl">{job.company}</Heading>
+        <Text>{job.title}</Text>
+        <Text>{job.time}</Text>
+      </Box>
+
+      <Box p={5}>
+        <UnorderedList>
+          {job.responsibilities.map((r) => (
+            <ListItem>{r}</ListItem>
+          ))}
+        </UnorderedList>
+      </Box>
+    </Stack>
+  );
+};
+
+const ShowStrength = ({ strength }: { strength: Strength }) => {
+  return (
+    <>
+      <Text>{strength.name}</Text>
+      {strength.items.map((i) => <Text>{i}</Text>)}
+    </>
+  );
+};
--- src/components/resume/resumeData.tsx
+++ src/components/resume/resumeData.tsx
@@ -1,15 +1,44 @@
-export const resumeData = {
+export const resumeData: Resume = {
   name: 'Joel Thompson',
   jobHistory: [
-    {
-      company: 'Intercom',
-      title: 'Software Engineer',
-      time: '2016 - 2020',
-    },
     {
       company: 'CircleCI',
       title: 'Senior Software Engineer',
       time: '2020 - Current',
-    }
-  ]
+      responsibilities: [
+        'Typed on a computer',
+        'Looked at screen',
+        'Looked at screen',
+        'Looked at screen',
+      ],
+    },
+    {
+      company: 'Intercom',
+      title: 'Software Engineer',
+      time: '2016 - 2020',
+      responsibilities: ['Typed on a computer'],
+    },
+  ],
+  strengths: [
+    { name: 'Languages', items: ['Typescript', 'React'] },
+    { name: 'Applications', items: ['Github', 'CircleCI'] },
+  ],
 };
+
+interface Resume {
+  name: string;
+  jobHistory: Job[];
+  strengths: Strength[];
+}
+
+export interface Job {
+  company: string;
+  title: string;
+  time: string;
+  responsibilities: string[];
+}
+
+export interface Strength {
+  name: string;
+  items: string[];
+}
